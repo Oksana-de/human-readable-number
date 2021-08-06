baa87b3897753324f3f80a838b8dd7eed11832c3
Oksana Vovere <oksana.vovere@gmail.com>
2021-08-06 15:14:13 +0200

    feat: finish task human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,47 +1,38 @@
 module.exports = function toReadable(number) {
 
-var words=["one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen"];
-var words_ten=["twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"];
+    let words = ["zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen"]
+    let words_ten = ["ten", "twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"]
+    let words_next = ["hundred", "thousand", "million"]
 
-if ((number>=100 && number<1000)) {
+    let txt = ''
 
-  if (((number%100)>=21 && (number&100)<100)) {
+    if (number <= 13) {
+        txt = words.find(element => words.indexOf(element) === number);
+    } else if (number === 15) {
+       txt = 'fifteen';
+    } else if (number === 18) {
+        txt = 'eighteen';
+    } else if (number >= 14 && number <= 19 && number !== 15 && number !==18) {
+        txt = `${words.find(element => words.indexOf(element) === number - 10)}teen`;
+    } else if (number >= 20 && number <= 99) {
+        number%10 === 0?txt = words_ten.find(element => words_ten.indexOf(element) + 1 === (number/10)):txt = `${words_ten.find(element => words_ten.indexOf(element)+1 === Math.floor(number/10))} ${words.find(element => words.indexOf(element) === number%10)}`;
+    } else if (number >= 100 && number <= 999) {
+        if (number%100 === 0) {
+            txt = `${words.find(element => words.indexOf(element) === (number/100))} ${words_next[0]}`;
+        } else if (number%10 === 0) {
+            txt = `${words.find(element => words.indexOf(element) === Math.floor(number/100))} ${words_next[0]} ${words_ten.find(element => words_ten.indexOf(element) + 1 === ((number%100)/10))}`;
+        } else if (number%100 >= 20 && number%100 <= 99) {
+            txt = `${words.find(element => words.indexOf(element) === Math.floor(number/100))} ${words_next[0]} ${words_ten.find(element => words_ten.indexOf(element) + 1 === Math.floor((number%100)/10))} ${words.find(element => words.indexOf(element) === number%10)}`;
+        } else if (number%100 === 18) {
+            txt = `${words.find(element => words.indexOf(element) === Math.floor(number/100))} ${words_next[0]} eighteen`;
+        } else if (number%100 === 15) {
+            txt = `${words.find(element => words.indexOf(element) === Math.floor(number/100))} ${words_next[0]} fifteen`;
+        }  else if (number%100 >= 14 && number%100 <= 19 && number%100 !== 15 && number%100 !== 18) {
+            txt = `${words.find(element => words.indexOf(element) === Math.floor(number/100))} ${words_next[0]} ${words.find(element => words.indexOf(element) === ((number%100) - 10))}teen`;
+        } else {
+            txt = `${words.find(element => words.indexOf(element) === Math.floor(number/100))} ${words_next[0]} ${words.find(element => words.indexOf(element) === number%100)}`;
+        }        
+    }
 
-  for (var i=0; i<((number%100)-20); i++) {
-    var txt=words[(Math.floor(number/100)-1)]+" hundred"+" "+words_ten[(Math.floor((number%100)/10)-2)]+" "+words[(((number%100)%10)-1)];
-      }
-} else if ((number%100)==20) {
-      var txt=(words[(Math.floor(number/100)-1)]+" hundred"+" twenty");
-} else if (((number%100)>=14 && (number%100)<=19)) {
-
-for (var i=0; i<((number%100)-10); i++) {
-    var txt=ords[(Math.floor(number/100)-1)]+" hundred"+" "+words[i]+"teen";
-      }
-    } else {
-for (var i=0; i<number; i++) {
-  var txt=ords[(Math.floor(number/100)-1)]+" hundred"+" "+words[i];
-}
-}
-
-} else {
-
-  if ((number>=21 && number<100)) {
-
-  for (var i=0; i<(number-20); i++) {
-    var txt=words_ten[(Math.floor(number/10)-2)]+" "+words[((number%10)-1)];
-      }
-} else if (number==20) {
-      var txt="twenty";
-} else if ((number>=14 && number<=19)) {
-
-for (var i=0; i<(number-10); i++) {
-    var txt=words[i]+"teen";
-      }
-    } else {
-for (var i=0; i<number; i++) {
-  var txt=words[i];
-}
-}
-}
-return txt;
+return txt
 }
